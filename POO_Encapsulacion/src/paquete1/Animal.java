820e8bdce5e616397b2a5c94e27bbb0427e818a5
Jorge <jorge.medi.08@gmail.com>
2025-12-09 20:14:58 -0600

    Modificador de acceso protected

---
+++ POO_Encapsulacion/src/paquete1/Animal.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class Animal {
+
+    protected void camina(){
+        System.out.println("Este animal camina");
+    }
+
+    public void maulla(){
+        System.out.println("Miau");
+    }
+}
---
+++ POO_Encapsulacion/src/paquete2/Gato.java
@@ -0,0 +1,14 @@
+package paquete2;
+
+import paquete1.Animal;
+
+public class Gato {
+
+    public static void main(String[] args) {
+
+        Animal gato1 = new Animal();
+
+        //gato1.camina();
+        gato1.maulla();
+    }
+}
