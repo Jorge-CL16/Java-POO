2aa76950fc0484e1bed116e92c75f52b5f3b5764
Jorge <jorge.medi.08@gmail.com>
2025-12-09 20:56:41 -0600

    Modificador de acceso default

---
+++ POO_Encapsulacion/src/paquete_Default/Animal.java
@@ -0,0 +1,11 @@
+package paquete_Default;
+
+public class Animal {
+    String nombre, raza;
+    int edad;
+
+    void datos(){
+        System.out.println("Nombre: " + nombre + "\n" +
+                "Raza: " + raza + "\n" + "edad: " + edad);
+    }
+}
---
+++ POO_Encapsulacion/src/paquete_Default/Perro.java
@@ -0,0 +1,10 @@
+package paquete_Default;
+
+public class Perro {
+    public static void main(String[] args) {
+        Animal perro1 = new Animal();
+        perro1.nombre = "Mack";
+
+        perro1.datos();
+    }
+}
